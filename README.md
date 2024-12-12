d1bd87caf280d23dc985080f4fba5eda76209419
Hunaid Shakoor <hunaidshakoor@Hunaids-MacBook-Air.local>
2024-12-12 12:01:00 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Tour of Go
+
+This repo uses "A Tour of Go" to understand and learn Golang
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	// "time"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("My favorite number is", rand.Intn(10))
+}
